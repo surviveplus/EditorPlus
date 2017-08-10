8aec813457e6c4528fffc573beb1beae41e96178
shinichi.koga <shinichi.koga@avanade.com>
2017-08-10 17:43:43 +0900

    Add EditorPlus for Project

---
+++ office2013/EditorPlus.OfficeAddIn.Project/EditorPlus.OfficeAddIn.Project/EditorPlusRibbon.Designer.vb
@@ -0,0 +1,96 @@
+﻿Partial Class EditorPlusRibbon
+    Inherits Microsoft.Office.Tools.Ribbon.RibbonBase
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New(ByVal container As System.ComponentModel.IContainer)
+        MyClass.New()
+
+        'Windows.Forms クラス作成デザイナーのサポートに必要です
+        If (container IsNot Nothing) Then
+            container.Add(Me)
+        End If
+
+    End Sub
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New()
+        MyBase.New(Globals.Factory.GetRibbonFactory())
+
+        'この呼び出しは、コンポーネント デザイナーで必要です。
+        InitializeComponent()
+
+    End Sub
+
+    'Component は、コンポーネント一覧に後処理を実行するために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'コンポーネント デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャはコンポーネント デザイナーで必要です。
+    'コンポーネント デザイナーを使って変更できます。
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.EditorPlus = Me.Factory.CreateRibbonTab
+        Me.EditSelectionGroup = Me.Factory.CreateRibbonGroup
+        Me.InsertTextButton = Me.Factory.CreateRibbonButton
+        Me.EditorPlus.SuspendLayout()
+        Me.EditSelectionGroup.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'EditorPlus
+        '
+        Me.EditorPlus.Groups.Add(Me.EditSelectionGroup)
+        Me.EditorPlus.Label = "Editor Plus"
+        Me.EditorPlus.Name = "EditorPlus"
+        '
+        'EditSelectionGroup
+        '
+        Me.EditSelectionGroup.Items.Add(Me.InsertTextButton)
+        Me.EditSelectionGroup.Label = "Edit Selection"
+        Me.EditSelectionGroup.Name = "EditSelectionGroup"
+        '
+        'InsertTextButton
+        '
+        Me.InsertTextButton.Image = Global.EditorPlus.OfficeAddIn.Project.My.Resources.Resources.InsertText
+        Me.InsertTextButton.Label = "Insert Text"
+        Me.InsertTextButton.Name = "InsertTextButton"
+        Me.InsertTextButton.ShowImage = True
+        '
+        'EditorPlusRibbon
+        '
+        Me.Name = "EditorPlusRibbon"
+        Me.RibbonType = "Microsoft.Project.Project"
+        Me.Tabs.Add(Me.EditorPlus)
+        Me.EditorPlus.ResumeLayout(False)
+        Me.EditorPlus.PerformLayout()
+        Me.EditSelectionGroup.ResumeLayout(False)
+        Me.EditSelectionGroup.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents EditorPlus As Microsoft.Office.Tools.Ribbon.RibbonTab
+    Friend WithEvents EditSelectionGroup As Microsoft.Office.Tools.Ribbon.RibbonGroup
+    Friend WithEvents InsertTextButton As Microsoft.Office.Tools.Ribbon.RibbonButton
+End Class
+
+Partial Class ThisRibbonCollection
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Friend ReadOnly Property EditorPlusRibbon() As EditorPlusRibbon
+        Get
+            Return Me.GetRibbon(Of EditorPlusRibbon)()
+        End Get
+    End Property
+End Class
---
+++ office2013/EditorPlus.OfficeAddIn.Project/EditorPlus.OfficeAddIn.Project/EditorPlusRibbon.vb
@@ -0,0 +1,58 @@
+﻿Imports EditorPlus.UI
+Imports Microsoft.Office.Tools.Ribbon
+Imports Net.Surviveplus.SakuraMacaron.Core
+Imports Net.Surviveplus.SakuraMacaron.OfficeAddIn.Project
+Imports Net.Surviveplus.SakuraMacaron.OfficeAddIn.UI
+
+Public Class EditorPlusRibbon
+
+    Private Sub EditorPlusRibbon_Load(ByVal sender As System.Object, ByVal e As RibbonUIEventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private insertTextToolWindow As ElementControlToolWindow(Of InsertText)
+
+    Private Sub InsertTextButton_Click(sender As Object, e As RibbonControlEventArgs) Handles InsertTextButton.Click
+
+        If Me.insertTextToolWindow Is Nothing Then
+            Dim c = New InsertText
+            AddHandler c.InsertButtonClick,
+                Sub(sender2, e2)
+
+                    Dim macaron As New ProjectMacaron(ThisAddIn.Current.Application)
+
+                    Dim insertAction As Action(Of TextActionsParameters) = Nothing
+                    Select Case e2.InsertTo
+                        Case InsertTo.Head, InsertTo.LineHead
+                            insertAction =
+                                Sub(a)
+                                    If Not (e2.SkipIfStartedOrEndWithText AndAlso a.Text.StartsWith(e2.Text)) Then
+                                        a.InsertBeforeText = e2.Text
+                                    End If
+                                End Sub
+                        Case InsertTo.End, InsertTo.LineEnd
+                            insertAction =
+                                Sub(a)
+                                    If Not (e2.SkipIfStartedOrEndWithText AndAlso a.Text.EndsWith(e2.Text)) Then
+                                        a.InsertAfterText = e2.Text
+                                    End If
+                                End Sub
+                    End Select
+
+                    Select Case e2.InsertTo
+                        Case InsertTo.Head, InsertTo.End
+                            macaron.ReplaceSelectionText(Nothing, insertAction)
+
+                        Case InsertTo.LineHead, InsertTo.LineEnd
+                            macaron.ReplaceSelectionParagraphs(Nothing, insertAction)
+                    End Select
+
+
+                End Sub
+
+            Me.insertTextToolWindow = New ElementControlToolWindow(Of InsertText)(c, "Insert Text")
+        End If
+
+        Me.insertTextToolWindow?.Show()
+    End Sub
+End Class
