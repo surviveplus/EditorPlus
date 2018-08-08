8eee23a3070744133d2e759fb9370f2c43e38501
surviveplus <shinichi.koga@avanade.com>
2018-08-08 14:39:46 +0900

    Remove comments

--- office2013/EditorPlus.OfficeAddIn.Outlook/EditorPlus.OfficeAddIn.Outlook/EditorPlusRibbon.vb
+++ office2013/EditorPlus.OfficeAddIn.Outlook/EditorPlus.OfficeAddIn.Outlook/EditorPlusRibbon.vb
@@ -17,18 +17,17 @@ Public Class EditorPlusRibbon
 
 #Region " Win32API Definitions "
 
-        ' TODO: Insert the code of Declare of DllImport. (see static code analysis CA1060)
+        '
+        ' Insert the code of Declare of DllImport. (see static code analysis CA1060)
+        '
         Declare Function GetActiveWindow Lib "user32" () As IntPtr
-        Declare Function FindWindow Lib "user32" Alias "FindWindow" (ByVal lpClassName As String, ByVal lpWindowName As String) As IntPtr
-
 
         Declare Function SetWindowPos Lib "user32" (ByVal hWnd As IntPtr, ByVal hWndInsertAfter As Integer, ByVal X As Integer, ByVal Y As Integer, ByVal cx As Integer, ByVal cy As Integer, ByVal wFlags As Integer) As IntPtr
-        public Const HWND_TOPMOST = -1      '「常に手前」にする
-        Public Const HWND_NOTOPMOST = -2    '「常に手前」を解除
-
-        Public Const SWP_SHOWWINDOW = &H40   '表示する
-        Public Const SWP_NOSIZE = &H1        'サイズを変更しない
-        Public Const SWP_NOMOVE = &H2        '位置を変更しない
+        Public Const HWND_TOPMOST = -1
+        Public Const HWND_NOTOPMOST = -2
+        Public Const SWP_SHOWWINDOW = &H40
+        Public Const SWP_NOSIZE = &H1
+        Public Const SWP_NOMOVE = &H2
 #End Region
 
     End Class
@@ -41,16 +40,7 @@ Public Class EditorPlusRibbon
 
         Dim b = CType(sender, RibbonToggleButton)
         Dim topMost = b.Checked
-
-        'MsgBox(topMost)
-
         Dim hwnd = NativeMethods.GetActiveWindow()
-        Debug.WriteLine("hwnd : " + hwnd.ToString())
-
-        'Dim inspector = ThisAddIn.Current.Application.ActiveInspector
-        'Dim caption = inspector.GetType().InvokeMember("caption", System.Reflection.BindingFlags.GetProperty, Nothing, inspector, Nothing).ToString()
-        'Dim hwnd2 = NativeMethods.FindWindow("rctrl_renwnd32\0", caption)
-        'Debug.WriteLine("hwnd2 : " + hwnd2.ToString())
 
         If topMost Then
             Dim r = NativeMethods.SetWindowPos(hwnd, NativeMethods.HWND_TOPMOST, 0, 0, 0, 0, NativeMethods.SWP_SHOWWINDOW Or NativeMethods.SWP_NOMOVE Or NativeMethods.SWP_NOSIZE)
