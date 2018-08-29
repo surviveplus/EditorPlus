3b827d814f9c396f3b1c9789941b86e06403e785
surviveplus <shinichi.koga@avanade.com>
2018-08-29 20:23:58 +0900

    Add prototype of gridview for Bulk

---
+++ prototype/BulkUI/BulkUI/Bulk.xaml.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Collections.ObjectModel
+
+Public Class Bulk
+    Private Sub UserControl_Loaded(sender As Object, e As RoutedEventArgs)
+
+
+        Dim items As New ObservableCollection(Of TaskItem)({
+            New TaskItem With {.Subtitle = "A"},
+            New TaskItem With {.Subtitle = "B"}
+            })
+
+        Me.inputDataGrid.ItemsSource = items
+
+    End Sub
+
+
+    Private Sub inputDataGrid_PreviewKeyDown(sender As Object, e As KeyEventArgs)
+
+        If e.Key = Key.Enter Then
+            With CType(sender, DataGrid)
+                If .CurrentColumn.DisplayIndex + 1 = .Columns.Count Then
+                    If .Items.Count = .SelectedIndex + 1 Then
+                        ' Do Nothing
+                    Else
+                        .CommitEdit()
+                        .SelectedIndex += 1
+                        .CurrentCell = New DataGridCellInfo(.Items(.SelectedIndex), .Columns(0))
+                        .BeginEdit()
+                        e.Handled = True
+                    End If
+
+                Else
+                    .CommitEdit()
+                    .CurrentColumn = .Columns(.CurrentColumn.DisplayIndex + 1)
+                    .BeginEdit()
+                    e.Handled = True
+                End If
+
+            End With
+
+        ElseIf e.Key = Key.V AndAlso
+            (Keyboard.Modifiers And ModifierKeys.Control) = ModifierKeys.Control Then
+
+            Dim text = Clipboard.GetText()
+
+            Dim toDueDate =
+                Function(v As String) As DateTime?
+                    Dim d As DateTime
+                    If DateTime.TryParse(v, d) Then
+                        Return d
+                    End If
+                    Return Nothing
+                End Function
+
+            Dim geText =
+                Function(values As IEnumerable(Of String), index As Integer) As String
+                    If values.Count >= index + 1 Then
+                        Return values(index).Trim()
+                    End If
+                    Return Nothing
+                End Function
+
+            Dim items =
+                From line In text.Split(vbCr)
+                Let values = line.Split(vbTab)
+                Let subtitle = geText(values, 0)
+                Where Not String.IsNullOrWhiteSpace(subtitle)
+                Let duedateText = geText(values, 1)
+                Select New TaskItem With {
+                    .Subtitle = subtitle,
+                    .DueDate = toDueDate(duedateText)
+                    }
+
+            For Each item In items
+                CType(Me.inputDataGrid.ItemsSource, ObservableCollection(Of TaskItem)).Add(item)
+            Next item
+            e.Handled = True
+
+        End If
+
+    End Sub
+
+
+End Class
+
+Public Class TaskItem
+    Public Property Subtitle As String
+    Public Property DueDate As DateTime?
+End Class
---
+++ prototype/BulkUI/BulkUI/My Project/AssemblyInfo.vb
@@ -0,0 +1,59 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Globalization
+Imports System.Resources
+Imports System.Windows
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("BulkUI")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("BulkUI")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: ComVisible(false)>
+
+'ローカライズ可能なアプリケーションのビルドを開始するには、
+'.vbproj ファイルの <UICulture>CultureYouAreCodingWith</UICulture> を
+'<PropertyGroup> 内部で設定します。たとえば、
+'ソース ファイルで英語 (米国) を使用している場合、<UICulture> を "en-US" に設定します。次に、
+'下の NeutralResourceLanguage 属性のコメントを解除し、下の行の "en-US" を
+'プロジェクト ファイルの UICulture 設定と一致するように更新します。
+
+'<Assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)>
+
+
+'ThemeInfo 属性は、テーマ固有および汎用のリソース ディクショナリがある場所を表します。
+'第 1 パラメーター: テーマ固有のリソース ディクショナリが置かれている場所
+'(リソースがページ、
+' またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
+
+'第 2 パラメーター: 汎用リソース ディクショナリが置かれている場所
+'(リソースがページ、
+'アプリケーション、テーマ固有のリソース ディクショナリに見つからない場合に使用されます)
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+
+
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("0b3b7b98-bb6f-485c-8f5c-3e558c3f799d")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      Revision
+'
+' すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
