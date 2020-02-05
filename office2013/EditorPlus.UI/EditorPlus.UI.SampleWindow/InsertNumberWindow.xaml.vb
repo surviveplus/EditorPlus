12792f7ccea624d11c82c84ce951b790214d9577
Shinichi Koga <shinichi.koga@avanade.com>
2020-02-05 10:22:44 +0900

    Update RadioButton Theme

---
+++ office2013/EditorPlus.UI/EditorPlus.UI.SampleWindow/InsertNumberWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Public Class InsertNumberWindow
+    Implements IHasUsercontrol
+
+    Public ReadOnly Property MainUserControl As UserControl Implements IHasUsercontrol.MainUserControl
+        Get
+            Return Me.InsertNumber
+        End Get
+    End Property
+End Class
--- office2013/EditorPlus.UI/EditorPlus.UI.SampleWindow/MainWindow.xaml.vb
+++ office2013/EditorPlus.UI/EditorPlus.UI.SampleWindow/MainWindow.xaml.vb
@@ -63,4 +63,18 @@
         Next w
 
     End Sub
+
+    Private Sub InsertNumberButton_Click(sender As Object, e As RoutedEventArgs)
+        Dim w As New InsertNumberWindow()
+
+        If TypeOf w Is IHasUsercontrol Then
+            With CType(w, IHasUsercontrol)
+                .MainUserControl.DataContext = OfficeThemeModel.Current
+                .MainUserControl.Resources.Apply(OfficeAccentColor.Current)
+            End With
+        End If
+
+        w.Show()
+        Me.openedWindows.Add(w)
+    End Sub
 End Class
