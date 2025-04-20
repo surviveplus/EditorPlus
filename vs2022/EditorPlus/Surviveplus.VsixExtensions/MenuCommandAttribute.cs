a068b23fff98c1b0c2bde0ea4964f60d69d0885b
SHIN-ICHI <surviveplus@live.jp>
2025-04-20 13:33:28 +0900

    Update References

---
+++ vs2022/EditorPlus/Surviveplus.VsixExtensions/MenuCommandAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Surviveplus.VsixExtensions
+{
+
+	/// <summary>
+	/// Visual Studio コマンドのイベントハンドラとして動作するメソッドであることを示す属性です。
+	/// この属性でマークされたメソッドが IServiceProviderExtensions.InitializeCommands で使用されます。
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method)] public sealed class MenuCommandAttribute : Attribute
+	{
+		/// <summary>
+		/// .vsct ファイルに記述したコマンドのIDを指定します。
+		/// </summary>
+		public int Id { get; private set; }
+
+		/// <summary>
+		/// MenuCommandAttribute クラスの新しいインスタンスを初期化します。
+		/// </summary>
+		/// <param name="id">.vsct ファイルに記述したコマンドのIDを指定します。</param>
+		public MenuCommandAttribute(int id) {
+			this.Id = id;
+		} // end constructor
+
+	} // end class
+} // end namespace
---
+++ vs2022/EditorPlus/Surviveplus.VsixExtensions/TextEditingArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Surviveplus.VsixExtensions
+{
+	/// <summary>
+	/// テキスト編集で扱うデータを司るクラスです。
+	/// </summary>
+	public class TextEditingArgs 
+	{
+		/// <summary>
+		/// テキストを取得または設定します。このテキストを変更すると、アクティブなテキストエディタの当該テキストが変更されます。
+		/// </summary>
+		public string Text { get; set; }
+
+		/// <summary>
+		/// テキスト編集を実施しない場合は True を設定します。それ以外は False を設定します。
+		/// </summary>
+		public bool Cancel { get; set; }
+
+	} // end class
+} // end namespace
